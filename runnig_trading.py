bc421e2a7092edd2b2034e9f7def3b23cdf93fa1
giminsu <bestiskim@gmail.com>
2023-08-25 19:04:25 +0900

    After generating the CCI signal, order one specific stock through Korea Investment & Securities’ API (limited to Korea and US markets)

--- runnig_trading.py
+++ runnig_trading.py
@@ -1,46 +1,94 @@
-import os 
-currentpath = os.getcwd()
+import sys
+from os import path
 import schedule
 import time
 import pause
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
 import exchange_calendars as ecals
-import pandas as pd
+from threading import Thread
 
-class BaseRunner():
-    def __init__(self, contry_code):
-        self.contry_code = contry_code
+app_path = path.abspath(path.join(".."))
+
+if app_path not in sys.path:
+    sys.path.append(app_path)
+
+from datetime import datetime, timedelta
+from create_cci_signal import trade_cci
+
+
+
+class BaseRunner:
+    def __init__(self):
+        pass
     
-    def check_time(self):
-        print(datetime.now())
-        print("오늘 영업합니다..")
-
-    def main(self):
-        str_now = datetime.now().strftime('%m-%d-%y %H:%M:%S')
-        print(f"Trading signal generation algorithm running! %s"%str_now)
-
+    def us_trade_runner(self):
+        str_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"US_trade_runner! %s"%str_now)
 
         while True:
-            if self.contry_code == "kr":
-                t_calendar = ecals.get_calendar("XKRX")
-                open_dummy = t_calendar.is_session(datetime.now().strftime("%Y-%m-%d"))
-                next_open_date = t_calendar.next_open(datetime.now()).date()
-                next_open_datetime = datetime.combine(next_open_date, datetime.strptime("1500", "%H%M").time())
-                
-            elif self.contry_code == "us":
-                t_calendar = ecals.get_calendar("XNYS")
-                open_dummy = t_calendar.is_session(datetime.now().strftime("%Y-%m-%d"))
-                next_open_date = t_calendar.next_open(datetime.today()).date()
-                next_open_datetime = datetime.combine(next_open_date, datetime.strptime("2359", "%H%M").time())
+            t_calendar = ecals.get_calendar("XNYS")
+            us_now = datetime.now() - timedelta(hours=13)
+            us_today_date = us_now.date()
+            open_dummy = t_calendar.is_session(us_now.strftime("%Y-%m-%d"))
+            next_open_timestamp = t_calendar.next_open(datetime.today())  ## type : pandas._libs.tslibs.timestamps.Timestamp
+            next_open_datetime = next_open_timestamp.to_pydatetime() + timedelta(hours=9)
+
+            trading_signal_time = "15:22"
+            signal_making_time = datetime.strptime(trading_signal_time, "%H:%M").time()
             
+            after_datetime = datetime.combine(us_today_date, signal_making_time) + timedelta(minutes=1)
+            until_time = datetime.combine(us_today_date, signal_making_time) - timedelta(minutes=3)
+
             if open_dummy == True:
-                schedule.every().days.at("23:21").do(self.check_time)
-            sleep_seconds = (next_open_datetime - datetime.now()).seconds
-            print(f"sleep until : {next_open_datetime}")
-            time.sleep(sleep_seconds)
+                if us_now < until_time:
+                    print(f"US Pause until_time {until_time + timedelta(hours=13)}")  ## 한국 시간으로 Pause time 표시
+                    pause.until(until_time + timedelta(hours=13))  ## 한국 시간으로 해야 Pause 걸림
+                elif us_now > until_time and us_now < after_datetime:
+                    schedule.every().days.at(trading_signal_time).do(trade_cci, price_target_market="NASDAQ", target_stock_code="AAL", cci_calculate_days=20, rate_of_one_order=0.1, order_type="34")
+                    time.sleep(58)
+                elif us_now > after_datetime:
+                    print(f"US Pause until_time {next_open_datetime}")
+                    pause.until(next_open_datetime)
+            else:
+                print(f"US Pause until_time {next_open_datetime}")
+                pause.until(next_open_datetime)
             
-            schedule.run_pending()    
-            time.sleep(59)
+            schedule.run_pending()
+            time.sleep(58)  
+
+    def kr_trade_runner(self):
+        str_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"KR_trade_runner! %s"%str_now)
+
+        while True:
+            t_calendar = ecals.get_calendar("XKRX")
+            open_dummy = t_calendar.is_session(datetime.now().strftime("%Y-%m-%d"))
+            next_open_timestamp = t_calendar.next_open(datetime.today())
+            next_open_datetime = next_open_timestamp.to_pydatetime() + timedelta(hours=9)
+
+            trading_signal_time = "15:22"
+            signal_making_time = datetime.strptime(trading_signal_time, "%H:%M").time()
+            after_datetime = datetime.combine(datetime.now().date(), signal_making_time) + timedelta(minutes=1)
+            until_time = datetime.combine(datetime.now().date(), signal_making_time) - timedelta(minutes=1)
+
+            if open_dummy == True:
+                now = datetime.now()
+                if now < until_time:
+                    print(f"KR Pause until_time {until_time}")
+                    pause.until(until_time)
+                elif now > until_time and now < after_datetime:
+                    schedule.every().days.at(trading_signal_time).do(trade_cci, price_target_market="KOSPI", target_stock_name="삼성전자우", cci_calculate_days=20, rate_of_one_order=0.1, order_type="01")
+                    time.sleep(59)
+                elif now > after_datetime:
+                    print(f"KR Pause until_time {next_open_datetime}")
+                    pause.until(next_open_datetime)
+            else:
+                print(f"KR Pause until_time {next_open_datetime}")
+                pause.until(next_open_datetime)
+            
+            schedule.run_pending() 
+            time.sleep(58)   
 
 if __name__ == "__main__":
-    BaseRunner(contry_code = "us").main()
+    Thread(target = BaseRunner().us_trade_runner).start()
+    Thread(target = BaseRunner().kr_trade_runner).start()
